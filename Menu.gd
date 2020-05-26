5d0f25fdaa51d082a7ab6da49e2510dc51bfcf77
Allen Shi <allen.shi@accenture.com>
2020-05-26 22:49:36 +1000

    today updates

--- Menu.gd
+++ Menu.gd
@@ -7,6 +7,13 @@ func _process(_delta):
 	#$earth.position += Vector2(0.05, 0)
 	#$earth.scale += Vector2(0.0001, 0.0001)
 	$earth.rotation_degrees += 0.05
+	var earth_degree = $earth.get_rotation_degrees()
+	if  earth_degree > 4 and earth_degree < 5:
+		$comet.set_emitting(true)
+	else:
+		$comet.set_emitting(false)
+		
 
 func _on_Button_pressed():
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://level/stage1/stage1_1.tscn")
