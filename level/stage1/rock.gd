f00f35b7dabfae1e7638f394994f56447b66418d
Allen Shi <allen.shi@accenture.com>
2020-06-13 21:31:58 +1000

    today updates

--- level/stage1/rock.gd
+++ level/stage1/rock.gd
@@ -13,14 +13,25 @@ func _ready():
 	
 	var point_count = collider_area.size()
 	for i in range(point_count):
+		#get new position after scaling
 		var p_point = position + collider_area[i] * collider_scale
+		#get new position after rotation
+#		var r = p_point.distance_to($sprite.position)
+#		var angle = p_point.angle_to_point($sprite.position) + deg2rad(get_rotation())
+#		var x = r * cos(abs(angle))
+#		var y = r * sin(abs(angle))
+#		p_point = Vector2(x, y)
+		#reset the postion after scaling and rotation
 		collider_area.set(i, p_point)
+
 
 		#get points postion after rotation
 		var r = p_point.distance_to($sprite.position)
-		var angle = p_point.angle_to_point($sprite.position) + $sprite.get_rotation()
-		var x = r * cos(abs(angle))
-		var y = r * sin(abs(angle))
-		print(String(i) + ":" + String(x) + "," + String(y))
+		var angle_ini = rad2deg(p_point.angle_to_point($sprite.position))
+		var angle_add = rad2deg(get_rotation())
+		var angle = angle_ini + angle_add
+		var x = r * cos(abs(deg2rad(angle)))
+		var y = r * sin(abs(deg2rad(angle)))
+		
 		print(String(p_point))
-		print(String(rad2deg(p_point.angle_to_point($sprite.position))))
+		print(String(i) + ":" + String(x) + "," + String(y))
